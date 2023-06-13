f8feb4ed6a9592fd5c10a929dde23bfc78367cab
JIPHEENS <JIPHEENS@gmail.com>
2023-06-13 23:46:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hotel-Reservation
+Steps:
+1. build the project
+2.  run
+3.  Load on the browser http://localhost:8090
+
+Note: The application automatically creates the database. You just need to make sure your MySQL Server is running
---
+++ src/test/java/com/hotel/HotelReservationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hotel;
+
+/*package henry.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+@Test
+void contextLoads() {
+}
+
+}*/
